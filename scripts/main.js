6a1d8de44b755558c03b98fb1074410dfe2afab2
cheyuexian <694547990@qq.com>
2017-07-23 23:19:04 +0800

    adding my files to respository

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+
+var myHeading=document.querySelector('h1')
+myHeading.innerHTML='hello world'
+
+var log=console.log.bind(console)
+
+var myImage=document.querySelector('img')
+myImage.onclick=function() {
+  var mySrc=myImage.getAttribute('src');
+  if(mySrc=='images/firefox.jpg'){
+    myImage.setAttribute('src',"images/chrome.jpg")
+  }else{
+    myImage.setAttribute('src',"images/firefox.jpg")
+
+  }
+}
+
+var myButton=document.querySelector('button')
+var myHeading=document.querySelector('h1')
+
+function setUserName() {
+  var myName = prompt('please enter your name.')
+  localStorage.setItem('name',myName)
+  myHeading.innerHTML = 'chrome is cool '+myName
+}
+if(!localStorage.getItem('name')){
+  setUserName();
+}else{
+  var  a=localStorage.getItem('name');
+  myHeading.innerHTML='hello,'+a;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
